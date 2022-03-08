03e8a722fa7210b5214a945d158a82397e1431cb
Eyrie48 <eddiie6@hotmail.com>
2022-03-08 09:58:22 -0800

    added block and filesys files

---
+++ Project1/block.cpp
@@ -0,0 +1,35 @@
+#pragma once
+#include "sdisk.h"
+#ifndef BLOCK
+#define BLOCK
+
+vector<string> block(string s, int b)//   s-buffer , b-blocksize
+{
+    vector<string> blocks;
+    int numberofblocks;
+
+    if(s.length()%b==0){
+        numberofblocks=s.length()/b;
+    }
+    else{
+        numberofblocks=s.length()/b+1;
+    }
+
+    string tempblock;
+    for(int i=0; i < numberofblocks; i++)
+    {
+        tempblock =s.substr(b*i, b);
+        blocks.push_back(tempblock);
+    }
+
+    //add #'s to 
+    int lastblock = blocks.size()-1;
+    for(int i = blocks[lastblock].length(); i < b; i++)
+    {
+        blocks[lastblock] += "#"; 
+    }
+
+    return blocks;
+}
+#endif
+//c++ -c block.cpp
---
+++ Project1/filesys.cpp
@@ -0,0 +1,42 @@
+#include "filesys.h"
+
+Filesys::Filesys(string filename, int numberofblocks, int blocksize)
+{
+
+}
+int Filesys::fsclose()
+{
+
+}
+int Filesys::newfile(string file)
+{
+
+}
+int Filesys::rmfile(string file)
+{
+
+}
+int Filesys::getfirstblock(string file)
+{
+
+}
+int Filesys::addblock(string file, string block)
+{
+
+}
+int Filesys::delblock(string file, int blocknumber)
+{
+
+}
+int Filesys::readblock(string file, int blocknumber, string& buffer)
+{
+
+}
+int Filesys::writeblock(string file, int blocknumber, string buffer)
+{
+
+}
+int Filesys::nextblock(string file, int blocknumber)
+{
+
+}
