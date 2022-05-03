5389fcf385d658c391249b4d9f7d7e7e0d1c319e
Eyrie48 <eddiie6@hotmail.com>
2022-05-03 09:12:45 -0700

    update files for last part

--- Project1/source.cpp
+++ Project1/source.cpp
@@ -27,6 +27,7 @@ int main()
 	*/
 /////////////////////////////////////
 
+/*
 	//Project 2
 	Sdisk disk1("disk1",256,128);
 	Filesys fsys("disk1",256,128);
@@ -69,7 +70,7 @@ int main()
 
 
 
-
+*/
 
 //////////////////////////////////////////////////
 //test delblock lab 5
@@ -112,5 +113,75 @@ int main()
 
 	fsys.delblock("file2",blocknumber);
 */
+
+/////////////////////////////////////////////
+	//last part
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+ //
+ //This main program inputs commands to the shell.
+ //It inputs commands as : command op1 op2
+ //You should modify it to work for your implementation.
+ //
+ Sdisk sdisk=Sdisk("sdisk.txt",256,128);
+ Filesys fsys=Filesys("sdisk.txt",256,128);
+ Shell shell=Shell("sdisk.txt",256,128);
+ Table table=Table("sdisk.txt",256,128);
+ table.build_table("data.txt");
+
+ string s;
+ string command="go";
+ string op1,op2;
+
+ while (command != "quit")
+	{
+       command.clear();
+       op1.clear();
+       op2.clear();
+
+       cout << "$";
+       getline(cin,s);
+       int firstblank=s.find(' ');
+	   
+       if (firstblank < s.length()) s[firstblank]='#';
+       int secondblank=s.find(' ');
+       command=s.substr(0,firstblank);
+       if (firstblank < s.length())
+    		op1=s.substr(firstblank+1,secondblank-firstblank-1);
+       if (secondblank < s.length())
+        	op2=s.substr(secondblank+1);
+       if (command=="dir")
+          {
+            // use the ls function
+			shell.dir();
+          }
+       if (command=="search")
+          {
+            // The variable op1 is the date
+          }
+       if (command=="add")
+          {
+            // The variable op1 is the new file
+          }
+       if (command=="del")
+          {
+            // The variable op1 is the file
+          }
+       if (command=="type")
+          {
+            // The variable op1 is the file
+          }
+       if (command=="copy")
+          {
+            // The variable op1 is the source file and the variable op2 is the destination file.
+          }
+       
+	}
+
+ return 0;
+
 	
 }
