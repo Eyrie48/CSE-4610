7f24214381ae7b3c93621e9fea876df79a95e87b
Edward A <64619589+Eyrie48@users.noreply.github.com>
2022-05-05 10:11:51 -0700

    Update source.cpp

--- Project1/source.cpp
+++ Project1/source.cpp
@@ -32,43 +32,29 @@ int main()
 	Filesys fsys("disk1",256,128);
 	fsys.newfile("file1");
 	fsys.newfile("file2");
-
 	string bfile1;
 	string bfile2;
-
 	for (int i=1; i<=1024; i++)
 	{
 		bfile1+="1";
 	}
-
 	vector<string> blocks=fsys.block(bfile1,128); 
-
 	int blocknumber=0;
-
 	for (int i=0; i< blocks.size(); i++)
 	{
 		blocknumber=fsys.addblock("file1",blocks[i]);
 	}
-
 	fsys.delblock("file1",fsys.getfirstblock("file1"));
-
 	for (int i=1; i<=2048; i++)
 	{
 		bfile2+="2";
 	}
-
 	blocks=fsys.block(bfile2,128); 
-
 	for (int i=0; i< blocks.size(); i++)
 	{
 		blocknumber=fsys.addblock("file2",blocks[i]);
 	}
-
 	fsys.delblock("file2",blocknumber);
-
-
-
-
 */
 
 //////////////////////////////////////////////////
@@ -78,47 +64,96 @@ int main()
 	Filesys fsys("disk1",256,128);
 	fsys.newfile("file1");
 	fsys.newfile("file2");
-
 	string bfile1;
 	string bfile2;
-
 	for(int i=1; i<=1024; i++)
 	{
 		bfile1+="1";
 	}
-
 	vector<string> blocks=block(bfile1,128); 
-
 	int blocknumber=0
-
 	for(int i=0; i< blocks.size(); i++)
 	{
 		blocknumber=fsys.addblock("file1",blocks[i]);
 	}
-
 	fsys.delblock("file1",fsys.getfirstblock("file1"));
-
 	for (int i=1; i<=2048; i++)
 	{
 		bfile2+="2";
 	}
-
 	blocks=block(bfile2,128); 
-
 	for (int i=0; i< blocks.size(); i++)
 	{
 		blocknumber=fsys.addblock("file2",blocks[i]);
 	}
-
 	fsys.delblock("file2",blocknumber);
 */
 
+
+/////////////////////////////////////////////////////
+	//part 3
+	//
+ //This main program inputs commands to the shell.
+ //It inputs commands as : command op1 op2
+ //You should modify it to work for your implementation.
+ //
+ string s;
+ string command="go";
+ string op1,op2;
+
+ while (command != "quit")
+     {
+       command.clear();
+       op1.clear();
+       op2.clear();
+       cout << "$";
+       getline(cin,s);
+       int firstblank=s.find(' ');
+       if (firstblank < s.length()) s[firstblank]='#';
+       int secondblank=s.find(' ');
+       command=s.substr(0,firstblank);
+       if (firstblank < s.length())
+         op1=s.substr(firstblank+1,secondblank-firstblank-1);
+       if (secondblank < s.length())
+         op2=s.substr(secondblank+1);
+       if (command=="dir")
+          {
+            // use the ls function
+	    shell.dir();
+           }
+       if (command=="add")
+          {
+            // The variable op1 is the new file and op2 is the file data
+	    shell.add(op1, op2);
+           }
+       if (command=="del")
+          {
+            // The variable op1 is the file
+	    shell.del(op1);
+           }
+       if (command=="type")
+          {
+            // The variable op1 is the file
+	    shell.type(op1);
+           }
+       if (command=="copy")
+          {
+            // The variable op1 is the source file and the variable op2 is the destination file.
+	    shell.copy(op1);
+           }
+       if (command=="search")
+          {
+            // This is the command for Project 4
+            // The variable op1 is the date
+           }
+       
+      }
+
+ return 0;
+/*
 /////////////////////////////////////////////
 	//last part
-#include <iostream>
-#include <string>
 
-using namespace std;
 
  //
  //This main program inputs commands to the shell.
@@ -128,8 +163,8 @@ using namespace std;
  Sdisk sdisk=Sdisk("sdisk.txt",256,128);
  Filesys fsys=Filesys("sdisk.txt",256,128);
  Shell shell=Shell("sdisk.txt",256,128);
- Table table=Table("sdisk.txt",256,128, "flatfile", "indexfile");
- table.build_table("data.txt");
+ //Table table=Table("sdisk.txt",256,128, "file2", "file1");
+ //table.build_table("data.txt");
 
  string s;
  string command="go";
@@ -162,12 +197,13 @@ using namespace std;
             // The variable op1 is the date
 			//getline(cin, op1);
 			//table.search(op1);
-			table.search(op1);
+			//table.search(op1);
           }
        if (command=="add")
           {
             // The variable op1 is the new file
-			shell.add(op1);
+	    shell.add(op1);
+			//fsys.addblock(op1, op2);
           }
        if (command=="del")
           {
@@ -188,6 +224,7 @@ using namespace std;
 	}
 
  return 0;
+*/
 
 	
 }
